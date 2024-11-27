a1b1859ca5220d927ee50308af19fc1582fa3f02
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2024-11-27 21:33:47 +0100

    Update dotfiles files

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
-  'folke/tokyonight.nvim',
-  lazy = false,
-  priority = 1000,
-  opts = {
-    style = "moon", -- styles: storm night moon
-  },
-  config = function(_, opts)
-    require('tokyonight').setup(opts)
-    vim.cmd("colorscheme tokyonight")
-  end
+	"catppuccin/nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		flavour = "mocha",
+	},
+	config = function(_, opts)
+		require("catppuccin").setup({
+			flavour = opts.flavour,
+		})
+		vim.cmd("colorscheme catppuccin")
+	end,
 }
