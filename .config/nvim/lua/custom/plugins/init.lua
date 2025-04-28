c91b1640e5411b98d99970df83439506e0b97d15
Wojciech Chuchro <wojciech.chuchro@icloud.com>
2025-04-28 17:09:44 +0200

    feat: update config

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -18,8 +18,8 @@ return {
       vim.opt.showtabline = 2
 
       -- Keymaps
-      vim.keymap.set('n', '<leader>ha', require('harpoon.mark').add_file, { desc = 'Harpoon add file' })
-      vim.keymap.set('n', '<C-e>', require('harpoon.ui').toggle_quick_menu, { desc = 'Harpoon toggle quick menu' })
+      vim.keymap.set('n', '<leader>ha', require('harpoon.mark').add_file)
+      vim.keymap.set('n', '<C-e>', require('harpoon.ui').toggle_quick_menu)
 
       -- Your highlight customizations (these are correctly placed)
       vim.cmd 'highlight! HarpoonInactive guibg=NONE guifg=#63698c'
@@ -31,31 +31,31 @@ return {
       -- Navigation keymaps
       vim.keymap.set('n', '<leader>1', function()
         require('harpoon.ui').nav_file(1)
-      end, { desc = 'Harpoon navigate to file 1' })
+      end)
       vim.keymap.set('n', '<leader>2', function()
         require('harpoon.ui').nav_file(2)
-      end, { desc = 'Harpoon navigate to file 2' })
+      end)
       vim.keymap.set('n', '<leader>3', function()
         require('harpoon.ui').nav_file(3)
-      end, { desc = 'Harpoon navigate to file 3' })
+      end)
       vim.keymap.set('n', '<leader>4', function()
         require('harpoon.ui').nav_file(4)
-      end, { desc = 'Harpoon navigate to file 4' })
+      end)
       vim.keymap.set('n', '<leader>5', function()
         require('harpoon.ui').nav_file(5)
-      end, { desc = 'Harpoon navigate to file 5' })
+      end)
       vim.keymap.set('n', '<leader>6', function()
         require('harpoon.ui').nav_file(6)
-      end, { desc = 'Harpoon navigate to file 6' })
+      end)
       vim.keymap.set('n', '<leader>7', function()
         require('harpoon.ui').nav_file(7)
-      end, { desc = 'Harpoon navigate to file 7' })
+      end)
       vim.keymap.set('n', '<leader>8', function()
         require('harpoon.ui').nav_file(8)
-      end, { desc = 'Harpoon navigate to file 8' })
+      end)
       vim.keymap.set('n', '<leader>9', function()
         require('harpoon.ui').nav_file(9)
-      end, { desc = 'Harpoon navigate to file 9' })
+      end)
     end,
   },
   {
