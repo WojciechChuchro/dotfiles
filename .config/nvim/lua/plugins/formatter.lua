7b3f439fc464f1235065f2eba227636170fa7265
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-07-24 20:42:15 +0200

    add sql formatter

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -4,29 +4,47 @@ return {
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
   config = function()
-    require("conform").setup({
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      go = { 'goimports' },
-      sql = { 'pg_format' },
-      python = { 'isort', 'black' },
-      javascript = { 'prettier' },
-      typescriptreact = { 'prettier' },
-      javascriptreact = { 'prettier' },
-      typescript = { 'prettier' },
-      json = { 'prettier' },
-      html = { 'prettier' },
-      css = { 'prettier' },
-      yaml = { 'prettier' },
-      java = { 'google-java-format' },
-    },
-    formatters = {
-      ['google-java-format'] = {
-        command = 'google-java-format',
-        args = { '--aosp', '-' }, -- Use AOSP style for 4-space indentation
+    require('conform').setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        go = { 'goimports' },
+        sql = { 'pg_format' },
+        python = { 'isort', 'black' },
+        javascript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        json = { 'prettier' },
+        html = { 'prettier' },
+        css = { 'prettier' },
+        yaml = { 'prettier' },
+        java = { 'google-java-format' },
       },
-    },
-    })
+      formatters = {
+        ['google-java-format'] = {
+          command = 'google-java-format',
+          args = { '--aosp', '-' }, -- Use AOSP style for 4-space indentation
+        },
+        ['pg_format'] = {
+          command = 'pg_format',
+          args = {
+            '--spaces',
+            '2', -- Number of spaces for indentation (default: 4)
+            '--maxlength',
+            '120', -- Maximum line length (default: 79)
+            '--comma-break', -- Add line break after comma in SELECT
+            '--wrap-after',
+            '3', -- Wrap after N items in SELECT
+            '--no-extra-line', -- Remove extra blank lines
+            '--keyword-case',
+            '2', -- Keywords: 0=no change, 1=lowercase, 2=uppercase
+            '--function-case',
+            '1', -- Functions: 0=no change, 1=lowercase, 2=uppercase
+          },
+          stdin = true,
+        },
+      },
+    }
 
     vim.keymap.set('n', '<leader>cf', function()
       require('conform').format { async = true, lsp_format = 'fallback' }
