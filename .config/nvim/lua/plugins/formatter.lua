7566cfc50995a1fb1720deebf4bb70373c2136ca
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-07-24 18:27:33 +0200

    fix formatter to properly load config

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -4,26 +4,8 @@ return {
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
   config = function()
-    vim.keymap.set('n', '<leader>f', function()
-      require('conform').format { async = true, lsp_format = 'fallback' }
-    end, { desc = 'Format buffer' })
-  end,
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        return nil
-      else
-        return {
-          timeout_ms = 500,
-          lsp_format = 'fallback',
-        }
-      end
-    end,
+
+    require("conform").setup({
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { 'goimports' },
@@ -45,5 +27,27 @@ return {
         args = { '--aosp', '-' }, -- Use AOSP style for 4-space indentation
       },
     },
+    })
+
+    vim.keymap.set('n', '<leader>cf', function()
+      require('conform').format { async = true, lsp_format = 'fallback' }
+    end, { desc = 'Format buffer' })
+  end,
+  opts = {
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
+      end
+    end,
   },
 }
