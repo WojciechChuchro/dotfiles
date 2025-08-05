b6d991fac03fdd2f89623b64bcc98ab196e9cc42
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-08-05 21:43:53 +0200

    use keys for setting keymap

--- .config/nvim/lua/plugins/custom/undotree.lua
+++ .config/nvim/lua/plugins/custom/undotree.lua
@@ -3,7 +3,8 @@ return {
   config = function()
     vim.g.undotree_WindowLayout = 2
     vim.g.undotree_SetFocusWhenToggle = 1
-    -- Optional keymap
-    vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<CR>', { desc = 'Toggle Undotree' })
   end,
+  keys = {
+    { '<leader>u', '<cmd>UndotreeToggle<CR>', desc = 'Toggle Undotree' },
+  },
 }
