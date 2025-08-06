6e57dc65ff9a15fe433893124cb8881d1800f7b9
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-08-06 05:54:14 +0200

    clean harpoon config

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -2,47 +2,44 @@ return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function(_, opts)
+  config = function()
     local harpoon = require 'harpoon'
-    harpoon:setup(opts)
+    harpoon:setup({})
   end,
 
   keys = function()
+    local harpoon = require 'harpoon'
     local keys = {
       {
         '<leader>H',
         function()
-          require('harpoon'):list():add()
-          vim.cmd 'redrawtabline' -- Refresh tabline after adding
+          harpoon:list():add()
         end,
         desc = 'Harpoon File',
       },
       {
         '<C-e>',
         function()
-          local harpoon = require 'harpoon'
           harpoon.ui:toggle_quick_menu(harpoon:list())
         end,
         desc = 'Harpoon Quick Menu',
       },
     }
 
-    -- Add Ctrl+1-9 mappings
     for i = 1, 9 do
       table.insert(keys, {
         '<C-' .. i .. '>',
         function()
-          require('harpoon'):list():select(i)
+          harpoon:list():select(i)
         end,
         desc = 'Harpoon to File ' .. i,
       })
     end
 
-    -- Add Ctrl+0 for 10th file
     table.insert(keys, {
       '<C-0>',
       function()
-        require('harpoon'):list():select(10)
+        harpoon:list():select(10)
       end,
       desc = 'Harpoon to File 10',
     })
