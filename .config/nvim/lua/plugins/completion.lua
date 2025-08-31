f666c2313a3a40e70a206cd07e6a10e603612b21
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-08-31 19:19:08 +0200

    update completion to show icons on the left

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -53,6 +53,7 @@ return {
     config = function()
       local cmp = require 'cmp'
       require('luasnip.loaders.from_vscode').lazy_load()
+      local lspkind = require 'lspkind'
 
       cmp.setup {
 
@@ -60,20 +61,13 @@ return {
         completion = {
           completeopt = 'menu,menuone,noinsert',
         },
-
         formatting = {
-          format = function(entry, vim_item)
-            if vim.tbl_contains({ 'path' }, entry.source.name) then
-              local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-
-              if icon then
-                vim_item.kind = icon
-                vim_item.kind_hl_group = hl_group
-                return vim_item
-              end
-            end
-            return require('lspkind').cmp_format { with_text = false }(entry, vim_item)
-          end,
+          fields = { 'kind', 'abbr', 'menu' },
+          format = lspkind.cmp_format {
+            mode = 'symbol',
+            maxwidth = 50,
+            ellipsis_char = '...',
+          },
         },
         snippet = {
           expand = function(args)
